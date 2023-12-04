cfb0e3d71a0f3dc0b2aa3bd4c137f04dbc5cb70f
fatihcaliss <fthcls@gmail.com>
2023-12-04 16:40:19 +0300

    create ke app components to display expenses

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+
+const ExpensesOutput = ({ expenses }) => {
+  return (
+    <View>
+      <ExpensesSummary />
+      <ExpensesList />
+    </View>
+  );
+};
+
+export default ExpensesOutput;
